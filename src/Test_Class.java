13bca8f911c24d2a89a3f05113beed5a1d54192e
Physicist-QA <89131470+Physicist-QA@users.noreply.github.com>
2021-08-18 16:33:52 +0200

    Update Test_Class.java

--- src/Test_Class.java
+++ src/Test_Class.java
@@ -1,6 +1,6 @@
 public class Test_Class {
     public static void main(String[] args) {
         System.out.println("Git den merhaba hosgeldiniz");
-
+        System.out.println("GitHub dan merhaba sizde hosgeldiniz");
     }
 }
